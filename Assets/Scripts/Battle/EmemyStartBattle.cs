112b62ed4896dce7fe4261e07969b731cf8d46bb
Madrobot9182 <65980347+Madrobot9182@users.noreply.github.com>
2025-01-22 21:27:57 -0700

    Transition into battle scene

---
+++ Assets/Scripts/Battle/EmemyStartBattle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EmemyStartBattle : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemyEncounterPrefab;
+    private bool spawning = false;
+    void Start () 
+    {
+        DontDestroyOnLoad(this.gameObject);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    private void OnSceneLoaded (Scene scene, LoadSceneMode mode) 
+    {
+        if(scene.name == "Battle") 
+        {
+            if(this.spawning) 
+            {
+                Instantiate(enemyEncounterPrefab);
+            }
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+            Destroy(this.gameObject);
+        }
+    }
+    void OnTriggerEnter2D (Collider2D other) 
+    {
+        Debug.Log("ENTERED WITH" + other.gameObject.name);
+        if(other.gameObject.CompareTag("Player")) 
+        {
+            this.spawning = true;
+            SceneManager.LoadScene("Battle Scene");
+        }
+    }
+}
---
+++ Assets/Scripts/Battle/UnitStats.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitStats : MonoBehaviour
+{
+    public float health;
+    public float attack;
+}
