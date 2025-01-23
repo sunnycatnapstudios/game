5fe51a260d2845d59930063190bde133b951c003
Madrobot9182 <65980347+Madrobot9182@users.noreply.github.com>
2025-01-23 16:19:18 -0700

    Redoing comabt from ground up

---
+++ Assets/Scripts/Battle/BattleHandler.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleHandler : MonoBehaviour
+{
+   // Expose this instance to other functions
+   private static BattleHandler instance;
+   public static BattleHandler GetInstance()
+   {
+      return instance;
+   }
+   
+   [SerializeField] private Transform pfCharacterBattle;
+   
+   private CharacterBattle playerCharacterBattle;
+   private CharacterBattle enemyCharacterBattle;
+
+   private State state;
+   
+   private enum State
+   {
+      WaitingForPlayer,
+      Busy,
+   }
+   private void Awake()
+   {
+      instance = this;
+   }
+
+   private void Start()
+   {
+      // TEMP
+      playerCharacterBattle = SpawnCharacter(true);
+      enemyCharacterBattle = SpawnCharacter(false);
+
+      state = State.WaitingForPlayer;
+   }
+
+   private void Update()
+   {
+      if (state == State.WaitingForPlayer)
+      {
+         if (Input.GetKeyDown(KeyCode.Space))   // TODO attack when pressing space bar for now
+         {
+            state = State.Busy;
+            playerCharacterBattle.Attack(enemyCharacterBattle, onAttackComplete: () =>
+            {
+               state = State.WaitingForPlayer;  // Return to waiting state after attack
+            });
+         }
+      }
+   }
+
+   private CharacterBattle SpawnCharacter(bool isPlayerTeam)
+   {
+      Vector3 position;
+      
+      // Spawn on either left or right hand side
+      if (isPlayerTeam)
+      {
+         position = new Vector3(-5, 0);
+      }
+      else
+      {
+         position = new Vector3(5, 0);
+      }
+      
+      Transform characterTransform = Instantiate(pfCharacterBattle, position, Quaternion.identity); // Spawn into scene
+      CharacterBattle characterBattle = characterTransform.GetComponent<CharacterBattle>();  // Retrieve the Character Battle instance
+      characterBattle.Setup(isPlayerTeam);
+      
+      return characterBattle;
+   }
+}
--- Assets/Scripts/Battle/UnitStats.cs
+++ Assets/Scripts/Battle/UnitStats.cs
@@ -1,9 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UnitStats : MonoBehaviour
 {
-    public float health;
-    public float attack;
+    public float health = 10;
+    public float attack = 1;
+    public float speed = 1;
+
+    public int nextActTurn;
+    private bool dead = false;
+
+    public void calculateNextActTurn(int currentTurn)
+    {
+        nextActTurn = currentTurn + (int)Math.Ceiling(100.0f / speed);
+    }
+    
+    public int CompareTo (object otherStats)
+    {
+        return nextActTurn.CompareTo(((UnitStats)otherStats).nextActTurn);
+    }
+
+    public bool isDead()
+    {
+        return dead;
+    }
 }
