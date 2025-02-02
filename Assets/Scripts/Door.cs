0f6a2bd9d2756868bc9cdf076f73f1a99b7b8121
Kaaden RumanCam <kaaden@ualberta.ca>
2025-02-02 05:25:28 -0700

    Player can interact when in front of door to open/close it

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -6,11 +6,12 @@ public class Door : MonoBehaviour {
     [HideInInspector] public SpriteRenderer spriteRenderer;
     public bool isOpen, isLocked;
     public Sprite closed, open;
+    bool interactable = false;
 
     // Start is called before the first frame update
     void Start() {
         spriteRenderer = GetComponent<SpriteRenderer>();
-	if (isOpen) {
+        if (isOpen) {
             Open();
         } else {
             Close();
@@ -18,6 +19,35 @@ public class Door : MonoBehaviour {
     }
 
     void Update() {
+        if (interactable && Input.GetKeyDown(KeyCode.E)) {
+            Interact();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collison) {
+        if (collison.gameObject.tag == "Player") {
+            interactable = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collison) {
+        if (collison.gameObject.tag == "Player") {
+            interactable = false;
+        }
+    }
+
+    void Interact() {
+        if (isLocked) {
+            if (true/* key */) {
+                Unlock();
+            }
+        } else {
+            if (isOpen) {
+                Close();
+            } else {
+                Open();
+            }
+        }
     }
 
     void Close() {
