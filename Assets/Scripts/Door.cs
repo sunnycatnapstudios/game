571d6b68606376f56ba8a9d4d39a07ec2c242ffd
Kaaden RumanCam <kaaden@ualberta.ca>
2025-02-02 04:49:02 -0700

    Door that we can walk through when open

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+    [HideInInspector] public SpriteRenderer spriteRenderer;
+    public bool isOpen, isLocked;
+    public Sprite closed, open;
+
+    // Start is called before the first frame update
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+	if (isOpen) {
+            Open();
+        } else {
+            Close();
+        }
+    }
+
+    void Update() {
+    }
+
+    void Close() {
+	spriteRenderer.sprite = closed;
+	isOpen = false;
+        this.gameObject.layer = LayerMask.NameToLayer("Can't Traverse");
+    }
+
+    void Open() {
+	spriteRenderer.sprite = open;
+	isOpen = true;
+        this.gameObject.layer = LayerMask.NameToLayer("Default");
+    }
+
+    void Lock() {
+        isLocked = true;
+    }
+
+    void Unlock() {
+	isLocked = false;
+    }
+}
