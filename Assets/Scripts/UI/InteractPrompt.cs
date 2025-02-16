281c23417adbfd4b04149a1b75f3367f07a96e4b
Madrobot9182 <65980347+Madrobot9182@users.noreply.github.com>
2025-02-16 12:05:37 -0700

    Changed order of import statement

--- Assets/Scripts/UI/InteractPrompt.cs
+++ Assets/Scripts/UI/InteractPrompt.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
 public class InteractPrompt : MonoBehaviour {
     public LayerMask playerLayer;
