5921a4cceb9050e54350444d9b7fd1734e342e92
Kaaden RumanCam <kaaden@ualberta.ca>
2025-02-19 23:55:51 -0700

    auto-format TypeWriter

--- Assets/Scripts/UI/TypeWriter.cs
+++ Assets/Scripts/UI/TypeWriter.cs
@@ -1,41 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
-public class TypeWriter : MonoBehaviour
-{
+public class TypeWriter : MonoBehaviour {
     TMP_Text _tmpProText;
-	string writer;
-    
+    string writer;
+
     [SerializeField] float delayBeforeStart = 0f;
-	[SerializeField] float timeBtwChars = 0.1f;
-	[SerializeField] string leadingChar = ""; // TODO: do we ever plan to use this? can it be removed?
-	[SerializeField] bool leadingCharBeforeDelay = false;
+    [SerializeField] float timeBtwChars = 0.1f;
+    [SerializeField] string leadingChar = ""; // TODO: do we ever plan to use this? can it be removed?
+    [SerializeField] bool leadingCharBeforeDelay = false;
 
-	public bool hasStartedTyping = false, isTyping = false, skipTyping = false;
-	public float textChirp;
+    public bool hasStartedTyping = false, isTyping = false, skipTyping = false;
+    public float textChirp;
 
-	public void StartTypewriter(string newText)
-	{
-		_tmpProText.text = "";
-		if (isTyping) StopAllCoroutines();
-		writer = newText;
-	}
+    public void StartTypewriter(string newText) {
+        _tmpProText.text = "";
+        if (isTyping) StopAllCoroutines();
+        writer = newText;
+    }
 
-    IEnumerator TypeWriterTMP()
-    {
-		isTyping = true;
+    IEnumerator TypeWriterTMP() {
+        isTyping = true;
         _tmpProText.text = leadingCharBeforeDelay ? leadingChar : "";
         yield return new WaitForSeconds(delayBeforeStart);
-		textChirp = 0f;
+        textChirp = 0f;
 
         for (int i = 0; i < writer.Length; ++i) {
-			if (skipTyping) {
-				_tmpProText.text = writer;
-				break;
-			}
+            if (skipTyping) {
+                _tmpProText.text = writer;
+                break;
+            }
 
             // If there is a style tag attach the whole thing
             if (writer[i] == '<') {
@@ -46,60 +43,54 @@ public class TypeWriter : MonoBehaviour
                         break;
                     }
                 }
-                _tmpProText.text += writer.Substring(start, i-start);
+                _tmpProText.text += writer.Substring(start, i - start);
                 if (i >= writer.Length) {
                     break;
                 }
             }
 
             char c = writer[i];
-			if (_tmpProText.text.Length > 0)
-			{
-				_tmpProText.text = _tmpProText.text.Substring(0, _tmpProText.text.Length - leadingChar.Length);
-			}
-			_tmpProText.text += c;
-			_tmpProText.text += leadingChar;
+            if (_tmpProText.text.Length > 0) {
+                _tmpProText.text = _tmpProText.text.Substring(0, _tmpProText.text.Length - leadingChar.Length);
+            }
+            _tmpProText.text += c;
+            _tmpProText.text += leadingChar;
 
-			textChirp+=.095f;
+            textChirp += .095f;
 
-			if (GetComponent<AudioSource>() != null && textChirp >= 0.2f) {
-				GetComponent<AudioSource>().Play();
-				textChirp = 0f;
-			}
-			yield return new WaitForSeconds(timeBtwChars);
-		}
+            if (GetComponent<AudioSource>() != null && textChirp >= 0.2f) {
+                GetComponent<AudioSource>().Play();
+                textChirp = 0f;
+            }
+            yield return new WaitForSeconds(timeBtwChars);
+        }
 
-		if (leadingChar != "")
-		{
-			_tmpProText.text = _tmpProText.text.Substring(0, _tmpProText.text.Length - leadingChar.Length);
-		}
-		isTyping = false; skipTyping = false;
+        if (leadingChar != "") {
+            _tmpProText.text = _tmpProText.text.Substring(0, _tmpProText.text.Length - leadingChar.Length);
+        }
+        isTyping = false; skipTyping = false;
     }
 
-    void Start()
-    {
+    void Start() {
         _tmpProText = GetComponent<TMP_Text>();
 
-        if (_tmpProText != null)
-		{
-			writer = _tmpProText.text;
-			_tmpProText.text = "";
+        if (_tmpProText != null) {
+            writer = _tmpProText.text;
+            _tmpProText.text = "";
 
-			// StartCoroutine("TypeWriterTMP");
-		}
+            // StartCoroutine("TypeWriterTMP");
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-		if (Input.GetKeyDown(KeyCode.E) && isTyping && _tmpProText.text.Length > 3) {skipTyping = true;}
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.E) && isTyping && _tmpProText.text.Length > 3) { skipTyping = true; }
 
         // Start typing only after activation and delay
-		if (hasStartedTyping && !isTyping)
-        {
+        if (hasStartedTyping && !isTyping) {
             hasStartedTyping = false;
-			StartCoroutine("TypeWriterTMP");
-			
+            StartCoroutine("TypeWriterTMP");
+
         }
     }
 }
